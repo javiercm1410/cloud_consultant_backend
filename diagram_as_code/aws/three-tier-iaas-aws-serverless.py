65d4eb56a94f5c7fb7cd15272dccf569cb8e33ec
Javier Carrillo <jamaca1410@gmail.com>
2023-04-12 15:49:31 -0400

    cahnges aa

--- diagram_as_code/aws/three-tier-iaas-aws-serverless.py
+++ diagram_as_code/aws/three-tier-iaas-aws-serverless.py
@@ -1,24 +1,22 @@
 from diagrams import Diagram, Cluster
 from diagrams.aws.database import Dynamodb
 from diagrams.aws.network import ALB
-from diagrams.aws.network import CloudFront
-from diagrams.aws.network import VPC
-from diagrams.aws.storage import S3
 from diagrams.aws.compute import Fargate
+from diagrams.aws.database import RDS
 
-with Diagram("Grouped Workers", show=False, direction="TB"):
-    with Cluster("VPC"):
-        CF = CloudFront("Cloud Front") 
+
+with Diagram("Grouped Workers", show=False):
+    with Cluster("Public"):
+        ALBFI = ALB("ALB")
         with Cluster ("Private Subnet"):
-            bucket = S3("Frontend")
-            ALBFI = ALB("ALB")
-            with Cluster ("ASG Web Tier"):
-                ECS_group = [Fargate("Web Tier"), 
-                            Fargate("Web Tier"), 
-                            Fargate("Web Tier")]
+            with Cluster ("Container Cluster"):
+                Fargate_group = [Fargate("Container"), 
+                            Fargate("Container"), 
+                            Fargate("Container")]
             with Cluster("Database Tier"):
-                    DB_group = Dynamodb("Amazon Aurora")
-                    DB_group - [Dynamodb("Worker")]
+                    DB_group = RDS("Amazon Aurora")
+
+                    # DB_group = Dynamodb("Amazon Aurora")
                 
-    CF >> bucket >> ALBFI >> ECS_group >> DB_group
+    ALBFI >> Fargate_group >> DB_group
      
