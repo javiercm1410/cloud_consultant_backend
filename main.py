93dafb7d526876bbe1ff56a5435bba49226ce482
Javier Carrillo <jamaca1410@gmail.com>
2023-04-13 01:18:54 -0400

    questi

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ def cloud_design_and_prices(cloud_provider_preference, workload, architecture_ty
         elif architecture_type == "Container-based" and database_type == "SQL":
             output = aws_container_three_tier_sql(workload, region, working_dir)
         else:
-            output = None
+            output = "None"
     elif region == "US_East" and cloud_provider_preference == "Azure":
         region = "eastus"
         if architecture_type == "Classic-three-tier" and database_type == "SQL":
             output = azure_classic_three_tier_sql(workload, auto_scale, region, working_dir)
         elif architecture_type == "Container-based" and database_type == "SQL":
-            output = None
+            output = "None"
         else:
-            output = None
+            output = "None"
     else:
-        Output = None
+        output = "None"
     return output 
 
-print(cloud_design_and_prices("AWS", "High", "Classic-three-tier", True, "SQL", "US_East"))
+# print(cloud_design_and_prices("AWS", "High", "Classic-three-tier", True, "SQL", "US_East"))
