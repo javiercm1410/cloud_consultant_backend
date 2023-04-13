574387596dd61b4b4045bb7d03acbe932c71ce7c
byFrederick <freddycruz5227@gmail.com>
2023-04-12 21:32:57 -0400

    .

--- diagram_as_code/aws/container_based_architecture_aws.py
+++ diagram_as_code/aws/container_based_architecture_aws.py
@@ -1,10 +1,14 @@
 from diagrams import Diagram, Cluster
-from diagrams.aws.database import Dynamodb
+# from diagrams.aws.database import Dynamodb
 from diagrams.aws.network import ALB
 from diagrams.aws.compute import Fargate
 from diagrams.aws.database import RDS
 
-
+def azure_container_based_architecture_diagram(working_dir):
+    web_app_name = "AWS Container Based Architecture"
+    output_path = f"{working_dir}/backend/images/"
+    filename = output_path + web_app_name.lower().replace(" ", "_")
+    
 with Diagram("Grouped Workers", show=False):
     with Cluster("Public"):
         ALBFI = ALB("ALB")
