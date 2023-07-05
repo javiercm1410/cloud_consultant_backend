8ff35b1371d567721f8084fd844a7e7c64712b61
Javier Carrillo <jamaca1410@gmail.com>
2023-07-05 18:08:58 -0400

    new api func

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require('cors');
 const path = require('path');
 const generateTopologyResponse = require('./topologyResponse');
+const generateDeployResponse = require('./deployResponse');
 
 const app = express();
 
@@ -27,6 +28,15 @@ app.post('/api/data', async function (req, res) {
   res.json(topologyResponse);
 });
 
+app.post('/api/deploy', async function (req, res) {
+  // Call the function to generate the response based on the request body
+  const deployResponse = await generateDeployResponse(req.body);
+
+  console.log(req.body);
+
+  // Send the generated response
+  res.json(deployResponse);
+});
 // // Serve the images using the /test-image route
 // app.get('/images/:filename', (req, res) => {
 //   const filename = req.params.filename;
